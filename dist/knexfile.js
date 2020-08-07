347251c979d9e7b3e7bd425cf8f47d0474015703
Wagner Silva <wagnerjps@gmail.com>
2020-08-06 21:30:08 -0300

    update

---
+++ dist/knexfile.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+module.exports = {
+    development: {
+        client: 'sqlite3',
+        connection: {
+            filename: './src/database/database.sqlite'
+        },
+        migrations: {
+            directory: './src/database/migrations'
+        },
+        useNullAsDefault: true,
+    },
+    production: {
+        client: 'sqlite3',
+        connection: {
+            filename: './dist/database/database.sqlite'
+        },
+        migrations: {
+            directory: './dist/database/migrations'
+        },
+        useNullAsDefault: true,
+    }
+};
