6454c42d0c234eeb680c758d47899be5bd3ef4eb
Wagner Silva <wagnerjps@gmail.com>
2020-08-08 03:55:59 -0300

    update postgres

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,25 @@
+require('dotenv').config();
 import path from 'path'
 
 module.exports = {
-    client: 'sqlite3',
-    connection: {
-        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+
+    development: {
+        client: 'sqlite3',
+        connection: {
+            filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+        },
+        migrations: {
+            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        useNullAsDefault: true,
     },
-    migrations: {
-        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-    },
-    useNullAsDefault: true,
-}
+
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            tableName: path.resolve(__dirname, 'src', 'database', 'migrations')
+        }
+        }
+
+};
